584f24c99c6a42bd13ce4103e79d4b799f9486ee
JMoney-216 <jdm216@gmail.com>
2020-01-22 16:26:43 -0600

    asdfd

--- src/pages/PuppyShowPage/PuppyShowPage.js
+++ src/pages/PuppyShowPage/PuppyShowPage.js
@@ -1,21 +1,7 @@
 import React, { Component } from "react";
 import "./PuppyShowPage.css";
-import puppyService from "../../utils/puppyService";
 import PuppyShowCard from "../../components/PuppyShowCard/PuppyShowCard";
 
-// export default class PuppyShowPage extends Component {
-//   state = {
-//     puppy: []
-//   };
-
-//   async componentDidMount(puppy) {
-//     console.log(puppy);
-
-//     const showPup = await puppyService.getPuppy(puppy._id);
-//     this.setState({ puppy: showPup });
-//   }
-
-//   render() {
 function PuppyShowPage(props) {
   const puppy = props.location.state.puppy;
 
